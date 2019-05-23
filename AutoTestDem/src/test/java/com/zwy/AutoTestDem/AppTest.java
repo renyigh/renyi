733dec3ee04cfdb7a0e1e376468f05b464189b09
renyihb <1048966401@qq.com>
2019-05-23 16:22:06 +0800

    提交

---
+++ AutoTestDem/src/test/java/com/zwy/AutoTestDem/AppTest.java
@@ -0,0 +1,27 @@
+package com.zwy.AutoTestDem;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest  extends before
+{
+    /**
+     * Rigorous Test :-)
+     * @throws InterruptedException 
+     */
+	
+	
+    @Test
+    public void apptest() throws InterruptedException
+    {
+ 
+    	
+    }
+  
+    
+		
+	
+}
